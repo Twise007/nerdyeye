bd754422431891079caf45ad2200b732d28325ea
Twise_Workshop <tundeoke80@gmail.com>
2022-04-13 14:56:44 +0100

    initial commit

---
+++ weekOne/app.js
@@ -0,0 +1,48 @@
+// // console.log('first statement')
+
+// // setTimeout(()=>{
+// //     console.log('second statement')
+// // }, 5000)
+
+// // console.log('thrid statement')
+
+// // const obj = {
+// //     name: 'Tunde',
+// //     profession: 'web dev'
+// // }
+
+// class People {
+//     constructor(name, profession, age){
+//         this.name = name
+//         this.profession = profession
+//         this.age = age
+//     }
+// }
+// const person = new People ('John Doe', 'Develop', 50)
+// console.log(person)
+// const person1 = new People ('Tunde', 'Network', 20)
+// console.log(person1)
+// const human = new People ('John ', 'Evelop', 40)
+// console.log(human)
+
+// const animal =[dog, cat, rat]
+// let pets = animal.toString();
+
+// const ani = animal.join('?');
+// console.log (pets)
+// console.log (ani)
+
+// const newArray = ani.split(`,`)
+// console.log(newArray)
+
+
+// Function Declearation
+// hello('ij')
+// function hello (name){
+//     console.log (`Hi ${name}`)
+// }
+
+
+
+
+
---
+++ weekOne/try.js
@@ -0,0 +1,13 @@
+const { modalUnstyledClasses } = require("@mui/material")
+
+const secret = `This is a secret`
+
+const person = {
+    name: "John Doe"
+}
+
+const sayHi = (name) => {
+    console.log (`Hi ${name}`)
+}
+
+module.exports = {person, sayHi}
